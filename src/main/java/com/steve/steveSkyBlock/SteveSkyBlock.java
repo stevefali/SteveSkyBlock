36b127b8765511ad375a9eb8afa8fa564ed6f459
Steve <stevefaliszewski@gmail.com>
2024-12-12 21:08:52 -0800

    Initial commit

---
+++ src/main/java/com/steve/steveSkyBlock/SteveSkyBlock.java
@@ -0,0 +1,17 @@
+package com.steve.steveSkyBlock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SteveSkyBlock extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
